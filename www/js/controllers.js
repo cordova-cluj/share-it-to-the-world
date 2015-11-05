57dbff7be873202a8cc930fa93632ff1bc091a17
cordova-cluj <dan.dobocan@wirtek.com>
2015-11-05 20:49:41 +0200

    Dan: add picture swipe

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,31 @@
 angular.module('starter.controllers', [])
 
 
-.controller('WelcomeCtrl', function($scope,$state) {
-   $scope.googleSignIn = function(){       
-       $state.go('tab.dash');
-   }
+.controller('WelcomeCtrl', function ($scope, $state) {
+    $scope.googleSignIn = function () {
+        $state.go('tab.dash');
+    }
 })
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function ($scope) { })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function ($scope) {
@@ -35,7 +35,42 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PicturesCtrl', function ($scope) {
-    $scope.settings = {
-        enableFriends: true
+
+    $scope.index = 0;
+
+    $scope.next = function () {
+        $scope.index++;
+        if ($scope.pictures.length <= $scope.index)
+            $scope.index = 0;
+
+        $scope.picture = $scope.pictures[$scope.index];
+    }
+
+    $scope.prev = function () {
+        $scope.index--;
+        if ($scope.index === -1)
+            $scope.index = $scope.pictures.length - 1;
+        $scope.picture = $scope.pictures[$scope.index];
+    }
+
+    $scope.picture = {
+        src: "img/arnold.jpg",
+        text: "Arnold"
     };
+
+    $scope.pictures =
+[
+    {
+        src: "img/arnold.jpg",
+        text: "Arnold"
+    },
+    {
+        src: "img/katherine.jpg",
+        text: "Katherine"
+    },
+    {
+        src: "img/ironMan.jpg",
+        text: "Iron Man"
+    }
+];
 });
